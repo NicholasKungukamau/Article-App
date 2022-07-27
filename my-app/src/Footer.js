55d0ec25991f8cada43406f8810ecdbb54975de9
NicholasKunguKamau <nicholaskungukamau@gmail.com>
2022-07-27 22:34:57 +0300

    Button to icrement likes

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -10,3 +10,11 @@ function Footer(props) {
       <p>
         This is the footer. <button onClick={() => dispatch({ type: "changeSize", value: 30 })}>Make the text 30px but leave the color the same</button>
       </p>
+       <p>
+        <button onClick={() => dispatch({ type: "incrementLikes" })}>Like The Page</button>
+      </p>
+    </footer>
+  )
+}
+
+export default React.memo(Footer)
