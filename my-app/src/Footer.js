60e34e6cf9382aeab8ef486749d25103d75e401b
NicholasKunguKamau <nicholaskungukamau@gmail.com>
2022-07-27 22:33:31 +0300

    Button to change pixels only

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -4,3 +4,9 @@ import DispatchContext from "../DispatchContext"
 function Footer(props) {
   console.log("Imagine our footer is slow.")
   const dispatch = React.useContext(DispatchContext)
+
+  return (
+    <footer className="footer">
+      <p>
+        This is the footer. <button onClick={() => dispatch({ type: "changeSize", value: 30 })}>Make the text 30px but leave the color the same</button>
+      </p>
