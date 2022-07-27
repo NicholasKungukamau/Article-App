996ef4d0b0610ac3f416b83d913e0fb3c2aed1c2
NicholasKunguKamau <nicholaskungukamau@gmail.com>
2022-07-27 22:40:08 +0300

    input values for color and pixel size

--- my-app/src/Sidebar.js
+++ my-app/src/Sidebar.js
@@ -5,3 +5,9 @@ import DispatchContext from "../DispatchContext"
 function Sidebar(props) {
   const state = React.useContext(OurContext)
   const dispatch = React.useContext(DispatchContext)
+
+  return (
+    <div className="sidebar">
+      <input type="text" value={state.size} onChange={e => dispatch({ type: "changeSize", value: e.target.value })} />
+      <input type="text" value={state.color} onChange={e => dispatch({ type: "changeColor", value: e.target.value })} />
+      
