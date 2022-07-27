36e31bae816422dc7823b3e2441e678bf671bb20
NicholasKunguKamau <nicholaskungukamau@gmail.com>
2022-07-27 22:37:55 +0300

    Sidebar function

--- my-app/src/Sidebar.js
+++ my-app/src/Sidebar.js
@@ -1,3 +1,7 @@
 import React from "react"
 import OurContext from "../OurContext"
 import DispatchContext from "../DispatchContext"
+
+function Sidebar(props) {
+  const state = React.useContext(OurContext)
+  const dispatch = React.useContext(DispatchContext)
